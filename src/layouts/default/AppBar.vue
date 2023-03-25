08f2577b94c9e1eae7a32df6ec906afcebdc93e1
leosch <leon.schoenhoffgw@outlook.com>
2023-03-25 15:47:54 +0100

    #3 setup vue project
    customize theme and setup project

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,25 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
-    </v-app-bar-title>
+  <v-app-bar :elevation="4" color="primary">
+    <v-app-bar-title>CRM</v-app-bar-title>
+    <template v-slot:append>
+      <v-btn @click="toggleTheme" color="secondary">Dark Mode</v-btn>
+    </template>
   </v-app-bar>
 </template>
 
-<script lang="ts" setup>
-  //
+<script lang="js">
+  import {useTheme} from "vuetify";
+
+  export default {
+    setup() {
+      const theme = useTheme();
+      return {
+        theme,
+        toggleTheme: () => {
+          theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+        }
+      }
+    }
+  }
+
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,6 +4,8 @@
  * Framework documentation: https://vuetifyjs.com`
  */
 
+import colors from 'vuetify/lib/util/colors'
+
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
@@ -16,10 +18,26 @@ export default createVuetify({
   theme: {
     themes: {
       light: {
+        dark: false,
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+          primary: colors.teal.lighten1,
+          secondary: colors.teal.lighten4,
+          warning: colors.amber.base,
+          success: colors.green.base,
+          info: colors.blue.base,
+          error: colors.red.base,
         },
+      },
+      dark: {
+        dark: true,
+        colors: {
+          primary: colors.teal.lighten1,
+          secondary: colors.teal.lighten4,
+          warning: colors.lime.accent1,
+          success: colors.lightGreen.base,
+          info: colors.blue.lighten4,
+          error: colors.deepOrange.base,
+        }
       },
     },
   },
