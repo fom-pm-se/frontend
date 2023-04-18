e6a275cb7bc9c86a0e072c0adf5db7f737ccaf69
Arthur <apache.arthur@gmail.com>
2023-04-18 23:32:06 +0200

    Einsatz/Nutzung v-model und v-form Steuert ob Reg-Button disabled wird

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col>
-      <v-form :disabled="isLoading" @submit.prevent>
+      <v-form v-model="isFormValid" :disabled="isLoading" @submit.prevent>
         <v-alert class="mb-5" type="error" v-if="usernameTaken" variant="outlined">Der Benutzer existiert bereits.</v-alert>
         <v-text-field label="Vorname" v-model="firstname" 
           :rules="[rules.required]"></v-text-field>
@@ -13,7 +13,7 @@
           :rules="[rules.min, rules.required, rules.requiresNumber, rules.requiresMixedCase]"></v-text-field>
         <v-text-field label="Passwort wiederholen" type="password" v-model="passwordRepeat" 
           :rules="[rules.mustMatchPassword]"></v-text-field>
-        <v-btn color="primary" :disabled="!isValid()" :loading="isLoading" @click="onSubmit" type="submit" >Registrieren</v-btn>
+        <v-btn color="primary" :disabled="!isFormValid" :loading="isLoading" @click="onSubmit" type="submit" >Registrieren</v-btn>
       </v-form>
     </v-col>
   </v-row>
@@ -32,6 +32,7 @@ export default defineComponent({
   },
   data() {
     return {
+      isFormValid: false,
       firstname: '',
       lastname: '',
       email: '',
@@ -49,18 +50,6 @@ export default defineComponent({
     }
   },
   methods: {
-    isValid(){
-      return false || (
-        this.firstname &&
-        this.lastname &&
-        /.+@.+\..+/.test(this.email) &&
-        /[A-Z]/.test(this.passwordRepeat) &&
-        /[a-z]/.test(this.passwordRepeat) &&
-        /\d/.test(this.passwordRepeat) &&
-        this.password == this.passwordRepeat &&
-        !this.usernameTaken
-        );
-    },
     validateUsername() {
       isUsernameAvailable(this.email).then((response) => {
         if (response.data) {
