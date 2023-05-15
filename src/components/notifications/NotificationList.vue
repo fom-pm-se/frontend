9566bdad10116f34a4529c4a60b65cdab9891701
Leon Schönhoff <49094676+leonschbbht@users.noreply.github.com>
2023-05-15 21:12:20 +0200

    remove unused button

--- src/components/notifications/NotificationList.vue
+++ src/components/notifications/NotificationList.vue
@@ -2,9 +2,6 @@
   <v-card width="300">
     <v-card-title>Benachrichtigungen</v-card-title>
     <v-card-subtitle>Du hast {{ notificationStore.notificationsCount }} neue Benachrichtigungen</v-card-subtitle>
-    <v-card-actions v-if="notificationStore.notificationsCount > 0">
-      <v-btn color="secondary" text prepend-icon="mdi-eye-outline">Alle als gelesen markieren</v-btn>
-    </v-card-actions>
     <v-divider class="mt-3" v-if="notificationStore.numberOfUnreadNotifications > 0"></v-divider>
     <v-list>
       <v-scroll-x-reverse-transition v-for="notification in notificationStore.notifications" :key="notification.id">
